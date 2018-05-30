7a69ef662890525030ecd56d591a0c8df3c8c090
jackblordbest <sysred2@hotmail.es>
2018-05-30 06:35:50 +0200

    Update EasyRender.js

--- EasyRender.js
+++ EasyRender.js
@@ -46,7 +46,145 @@ EasyRender.renderizar = function (objeto) {
 	
 	
 
+'use strict';
 
+/*
+	Adrián Fernández 30/05/2018
+
+	Ultima actualización: 30/05/2018 Versión: 0.1
+*/
+
+
+
+//config
+var EasyRender = {
+	'Marca_I' : '{{',
+	'Marca_F' : '}}',
+	'Scrips_Cargados': []
+};
+	
+
+
+//Renderiza el HTML y ejecuta la función contenida
+EasyRender.Renderizar = function (T, callback, salida) {
+	
+
+		var num = T.split(EasyRender.Marca_I).length - 1,
+
+		final = "";
+
+ 
+	for (var i = 0; i < num; i++) {
+ 
+
+
+		var	O = T.substring(T.indexOf(EasyRender.Marca_I) + EasyRender.Marca_I.length ,T.indexOf(EasyRender.Marca_F));
+
+
+		if (O.indexOf("return") != -1) {
+
+			var I = O.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&gt;/g, ">");
+
+		} else {
+
+			var I = "return " + O.replace(/[\n\r]/g, '') ;
+
+		}
+
+		//console.log( new Function (I));
+
+		T = T.replace(EasyRender.Marca_I + O + EasyRender.Marca_F, new Function (I)() || "");
+
+	}
+
+
+	console.log(typeof salida);
+
+	if (typeof salida == "undefined") {
+		entrada.innerHTML = T;
+	} else {
+		salida.innerHTML = T;
+	}
+	
+	
+	return callback(true);
+	
+};
+	
+	
+	
+
+
+
+
+
+//Inserto un script mediante url
+EasyRender.ImportarScript = function (nombre, callback) {
+	
+	try {
+		if (EasyRender.Scrips_Cargados.indexOf(nombre) != -1) {
+				return callback(true);
+		};
+
+		var s = document.createElement("script");
+		s.onload = callback;
+		s.src = nombre;
+		s.onerror = function(Error) {
+			return callback(Error);
+			console.log(Error);
+		};
+		s.setAttribute("id", nombre);
+		document.querySelector("head").appendChild(s);
+		
+		EasyRender.Scrips_Cargados.push(nombre);
+    } catch (e) {
+    	console.log(e);
+    }
+
+
+	
+};	
+	
+	
+
+
+//Elimina script ya cargados
+EasyRender.EliminarScript = function (nombre) {
+	
+	  try {
+        var index = EasyRender.Scrips_Cargados.indexOf(nombre);
+        if (index > -1) {
+            document.getElementById(nombre).remove();
+            EasyRender.Scrips_Cargados.splice(index, 1);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+
+};	
+
+	
+
+
+//Añado un stylesheet al documento
+EasyRender.ImportarCss = function (nombre, callback) {
+	
+	try {
+
+		var s = document.createElement('link');
+		s.rel = 'stylesheet';
+		s.type = 'text/css'; //opcional
+		s.href = nombre;
+		s.onload = callback;
+		document.querySelector("head").appendChild(s);
+
+    } catch (e) {
+        console.log(e);
+    }
+
+};	
+	
+	
 
 
 
